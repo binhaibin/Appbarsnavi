d426250e4b7dbaa45a5695d0ef3e9d14dca3823f
unknown <binhaibin>
2018-10-23 10:33:08 +0700

    asdsads

---
+++ Appbarsnavi/PHotoGallery.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Appbarsnavi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PHotoGallery : Page
+    {
+        public PHotoGallery()
+        {
+            this.InitializeComponent();
+        }
+
+        private void back_bth(object sender, RoutedEventArgs e)
+        {
+            asssss.Navigate(typeof(MainPage));
+        }
+
+        private async void get_pt(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                this.Name.Text =   file.Name;
+                this.path.Text = file.Path;
+                this.create.Text = file.DateCreated.ToString();
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                var image = new BitmapImage();
+                image.SetSource(stream);
+                X_image.Source = image;
+            }
+            else
+            {
+                this.Name.Text = "Operation cancelled.";
+
+            }
+        }
+    }
+}
